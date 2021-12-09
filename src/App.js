7e557f3693ff0ab3e3b878cc9b380df2bf84a50f
Goran Dejanovic <gorandejanovic@gmail.com>
2021-12-09 15:21:39 +0100

    first commit of zadaca02 JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+const x = 4;
+const y = 9;
+const element = <div>Hola!</div>
+const element2 = <div>I have {x} cars.</div>
+const element3 = <div>Numbers are {x} {y}.</div>
 
 function App() {
   return (
@@ -7,7 +12,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         {element}{element2}{element3}
         </p>
         <a
           className="App-link"
